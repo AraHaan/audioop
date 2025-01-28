f82e89da573aee3b8caf85903dc38d5f385bc40c
AraHaan <seandhunt_7@yahoo.com>
2025-01-28 08:39:00 -0500

    Cleanup build and egg-info directories after build.
    Also include the header files when ``py -m build --sdist`` is run.

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
+import shutil
+from pathlib import Path
 import sysconfig
 from setuptools import Extension, setup
 
 # run: py -m pip install -e .
+
+def cleanup():
+    # Get the directory where the setup.py is located
+    root_dir = Path(__file__).parent.resolve()
+    # Clean up the build and the egg-info directories.
+    dirs = [root_dir / 'build', *root_dir.rglob('*.egg-info')]
+    [shutil.rmtree(dir) for dir in dirs if dir.exists() and dir.is_dir()]
+
 
 Py_GIL_DISABLED = sysconfig.get_config_var("Py_GIL_DISABLED")
 options = {}
@@ -19,4 +29,6 @@ setup(
             define_macros=[("Py_LIMITED_API", "0x030D0000")] if not Py_GIL_DISABLED else [],
             py_limited_api=not Py_GIL_DISABLED)],
     options=options,
+    include_package_data=True
 )
+cleanup()
